80c08fc75b2bb74b1588c38930de5742e0a1c9c5
Hidalgo71 <y.ozdemir71@gmail.com>
2020-03-25 14:47:35 +0300

    Not work yet.

--- src/NLPlibraries/trTokenizer.java
+++ src/NLPlibraries/trTokenizer.java
@@ -2,21 +2,31 @@ package NLPlibraries;
 
 import org.antlr.v4.runtime.Token;
 import zemberek.tokenization.TurkishTokenizer;
+import zemberek.tokenization.antlr.TurkishLexer;
 
 import java.io.IOException;
 import java.util.List;
 
 public class trTokenizer
 {
+    public enum Type
+    {
+        Email,
+        HashTag,
+        Mention,
+        MetaTag,
+        URL,
+    }
+
     public static void main(String[] args)  throws IOException
     {
         TurkishTokenizer tokenizer = TurkishTokenizer.DEFAULT;
-        List<org.antlr.v4.runtime.Token> tokens = tokenizer.tokenize("Atatürk nerde doğdu?");
+        List<org.antlr.v4.runtime.Token> tokens = tokenizer.tokenize("google.com");
         for (Token token : tokens) {
-            System.out.println("Content = "  + token.getText());
-            System.out.println("Start = "  + token.getStartIndex());
-            System.out.println("Stop = " + token.getStopIndex());
-            System.out.println("Type = " + token.getType());
+            System.out.println("Content = " + token.getText());
+            System.out.println("Type = " + TurkishLexer.VOCABULARY.getDisplayName(token.getType()));
+            System.out.println("Start = " + token.getStartIndex());
+            System.out.println("Stop  = " + token.getStopIndex());
             System.out.println();
         }
     }
