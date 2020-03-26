4db08aed07ab38b6b86ecf0c77f5fd20ee9f0722
Hidalgo71 <y.ozdemir71@gmail.com>
2020-03-26 20:25:45 +0300

    Not work yet.

--- src/NLPlibraries/trTokenizer.java
+++ src/NLPlibraries/trTokenizer.java
@@ -1,6 +1,7 @@
 package NLPlibraries;
 
 import org.antlr.v4.runtime.Token;
+import zemberek.tokenization.TurkishSentenceExtractor;
 import zemberek.tokenization.TurkishTokenizer;
 import zemberek.tokenization.antlr.TurkishLexer;
 
@@ -20,7 +21,16 @@ public class trTokenizer
 
     public static void main(String[] args)  throws IOException
     {
+        TurkishSentenceExtractor extractor = TurkishSentenceExtractor.DEFAULT;
         TurkishTokenizer tokenizer = TurkishTokenizer.DEFAULT;
+
+        String input = "Merhaba! Bugün 2. köprü Fsm.'de trafik vardı.değil mi?";
+        List<String> sentences = extractor.fromParagraph(input);
+
+        sentences -> "hi", "bugğn 2.kopru", "değil mi?";
+
+        System.out.println("=============================");
+
         List<org.antlr.v4.runtime.Token> tokens = tokenizer.tokenize("google.com");
         for (Token token : tokens) {
             System.out.println("Content = " + token.getText());
